3a773e46848a59cdb77612e4ab70e3228717a026
MinKuk Jo <strike0115@naver.com>
2020-01-06 17:01:53 +0900

    Initial commit

---
+++ src/main/Greedy/GreedyFlorist/GreedyFlorist.java
@@ -0,0 +1,43 @@
+package main.Greedy.GreedyFlorist;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+    3 3
+    2 5 6
+    13
+
+    문제와 예제가 이해가 안되서 애먹은 문제.
+    친구가 꽃을 하나씩 사는데 다음 사는 꽃은 이전 산 갯수 +1 * 가격이 매겨진다.
+    풀이 자체는 간단한데 문제 이해가 어려워서 애먹었다.
+ */
+
+public class GreedyFlorist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+        int mul = 0;
+        int ans = 0;
+        int[] c = new int[n];
+        for(int i=0; i<n; i++){
+            c[i] = sc.nextInt();
+        }
+
+        Arrays.sort(c);
+
+        for(int i=n-1; i>=0; i--){
+            ans += (mul+1)*c[i];
+            count++;
+            if( count%k == 0){
+                mul++;
+            }
+        }
+        System.out.println(ans);
+
+
+
+    }
+}
---
+++ src/main/Greedy/MaxMin/MaxMin.java
@@ -0,0 +1,45 @@
+package main.Greedy.MaxMin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+    7
+    3
+    10
+    100
+    300
+    200
+    1000
+    20
+    30
+
+    20
+
+    문제를 1차원적으로 접근했다가 틀린 문제.
+    접근방법은 맞았다. 우선 배열을 소팅해준 뒤
+    범위만큼 슬라이드 하면서 범위의 가장 작은 인덱스와 가장 큰 인덱스에 위치한 값을 뺀 값을 이전값과 계속 비교해나가면 된다.
+    문제는 조합을 1개만 생각했는데, 계속 이동시키면서 해야했다.
+    자바 더블 피벗 소팅이 O(nlogn)이니까 총 시간 복잡도는 O(nlogn)
+ */
+
+public class MaxMin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        int ans = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i=0; i<=n-k; i++){
+            ans = Math.min(arr[k+i-1] - arr[i],ans);
+        }
+        System.out.println(ans);
+
+
+    }
+}
