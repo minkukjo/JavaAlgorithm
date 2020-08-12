08d7f634ee0e262947dd211807382299c559f6f2
minkukjo <strike0115@naver.com>
2020-08-12 22:05:01 +0900

    카카오 개발자 겨울 인턴십 문제 크레인 인형뽑기

---
+++ src/main/BaekJoon/N9012.java
@@ -0,0 +1,12 @@
+package main.BaekJoon;
+
+import java.util.Scanner;
+
+public class N9012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+
+    }
+}
